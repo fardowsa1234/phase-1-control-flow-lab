f7e650abdc3d29c8d9c72c1f44fb283720e8f325
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-03-22 08:12:36 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,94 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else {
+      // Return a different message for rides over 400 feet
+      return 'I will gladly take your thirty bucks.';
+  }
+}function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else if (feet > 2500) {
+      return 'No can do.';
+  } else {
+      return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case 'generous':
+          return 'Thank you so much.';
+      case 'not as generous':
+          return 'Thank you.';
+      default:
+          return 'Bye.';
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      return 'No can do.';
+  }
+}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      return 'No can do.';
+  } else {
+      return 'No can do.';
+  }
+}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      return 'No can do.';
+  } else {
+      return 'I will gladly take your thirty bucks.';
+  }
+}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else {
+      return 'No can do.';
+  }
+}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+      return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      return 'No can do.';
+  } else {
+      return 'No can do.';
+  }
+}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+      return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else {
+      return 'No can do.';
+  }
 }
